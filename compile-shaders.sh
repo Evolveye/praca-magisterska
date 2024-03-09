ee10b0c044db467226ee4f2fbeea4f5febb8ea21
evolveye <stolarski.paw@gmail.com>
2024-03-09 20:30:29 +0100

    move shaders compilator to root

---
+++ compile-shaders.sh
@@ -0,0 +1,2 @@
+glslc ./src/renderer/shaders/shader.vert -o ./src/renderer/shaders/vert.spv
+glslc ./src/renderer/shaders/shader.frag -o ./src/renderer/shaders/frag.spv
--- src/renderer/shaders/compile.sh
+++
@@ -1,2 +0,0 @@
-glslc ./src/renderer/shader.vert -o ./src/renderer/vert.spv
-glslc ./src/renderer/shader.frag -o ./src/renderer/frag.spv
